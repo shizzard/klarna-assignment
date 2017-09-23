d7b949706d8ce86ea8133948dc89b9364c229787
Denis Fakhrtdinov <me@shizz.ru>
2017-09-23 14:31:14 +0300

    first assignment

---
+++ apps/first/src/first.erl
@@ -0,0 +1,36 @@
+-module(first).
+
+-export([solution/1]).
+
+
+%% Interface
+
+
+-spec solution(Num :: non_neg_integer()) ->
+    Ret :: non_neg_integer().
+
+solution(Num)
+when is_integer(Num) andalso Num >= 0 ->
+    solution_loop(Num - 1, 0);
+
+solution(_Num) ->
+    throw(badarg).
+
+
+%% Externals
+
+
+-spec solution_loop(CurrentNum :: non_neg_integer(), Acc :: non_neg_integer()) ->
+    Ret :: non_neg_integer().
+
+solution_loop(CurrentNum, Acc)
+when CurrentNum >= 3
+andalso (CurrentNum rem 3 == 0 orelse CurrentNum rem 5 == 0) ->
+    solution_loop(CurrentNum - 1, Acc + CurrentNum);
+
+solution_loop(CurrentNum, Acc)
+when CurrentNum >= 3 ->
+    solution_loop(CurrentNum - 1, Acc);
+
+solution_loop(_CurrentNum, Acc) ->
+    Acc.
